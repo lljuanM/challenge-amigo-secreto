c4ed0f3686d35771a33403e6eab5116fecd3163b
Juan Montilla <lljuan9706@gmail.com>
2025-09-22 14:52:23 -0500

    se crea función para actualizar lista de amigos

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+function actualizarListaAmigos(){
+    //Limpia la lista de amigos
+        let lista = document.getElementById('listaAmigos');
+        lista.innerHTML = "";
+        //Crea la lista de amigos
+        for(i=0;i < amigos.length; i++){
+            const li = document.createElement("li"); //Crea elemento HTML de tipo li 
+            li.textContent = amigos[i]; // Asignar el nombre a la etiqueta li
+            lista.appendChild(li); // Agregar el <li> a la lista
+        }
+        //Por si ya sorteo una vez limpia el anterior sorteo y permite mostrar la lista de amigos
+        const resultado = document.getElementById("resultado");
+        resultado.innerHTML = "";
+}
+
 function agregarAmigo(){
     let nombreAmigo = document.getElementById('amigo').value;
         do{
